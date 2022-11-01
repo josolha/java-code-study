5733cb1242b84c724ccb86728387d884087317dd
bearjun05 <bearjun05@naver.com>
2022-11-01 19:16:05 +0900

    Initial commit

---
+++ src/main/java/comparator_comparable/Problem1.java
@@ -0,0 +1,17 @@
+package comparator_comparable;
+
+public class Problem1 {
+    
+
+}
+
+class Snack {
+
+    public String name;
+    public int price;
+
+    public Snack(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
---
+++ src/test/java/comparator_comparable/ApplicationTest.java
@@ -0,0 +1,12 @@
+package comparator_comparable;
+
+import org.junit.jupiter.api.Nested;
+
+public class ApplicationTest {
+
+    @Nested
+    class Problem1Test {
+
+
+    }
+}
